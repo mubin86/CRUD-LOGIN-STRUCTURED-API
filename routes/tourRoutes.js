82c7a8d101f68e94401016fec7248e7598acfa55
mubin86 <rahman.mmubinur@gmail.com>
2020-09-06 01:31:29 +0600

    authentication with JWT

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const tourController = require("./../controllers/tourController");
-
 const router = express.Router();
+const tourController = require("./../controllers/tourController");
+const authController = require("./../controllers/authController");
 
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(authController.protect, tourController.createTour);
 
 router
   .route("/:id")
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .get(authController.protect, tourController.getTour)
+  .patch(authController.protect, tourController.updateTour)
+  .delete(authController.protect, tourController.deleteTour);
 
 module.exports = router;
